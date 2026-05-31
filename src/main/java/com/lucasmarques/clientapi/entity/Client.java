124d951ed1958c28f1006c68ff2eacc1128942ef
Lucas Marques <lucaamarques2406@gmail.com>
2026-05-30 23:11:43 -0300

    feat: implements Bean Validation to class Client

--- src/main/java/com/lucasmarques/clientapi/entity/Client.java
+++ src/main/java/com/lucasmarques/clientapi/entity/Client.java
@@ -1,7 +1,12 @@
 package com.lucasmarques.clientapi.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NonNull;
+import org.springframework.context.annotation.Bean;
 
 
 import java.time.LocalDate;
@@ -15,15 +20,20 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @NotBlank
     @Column(nullable = false)
     private String name;
 
+    @Email
+    @NotBlank
     @Column(nullable = false)
     private String email;
 
+    @NotBlank
     @Column(nullable = false)
     private String cpf;
 
+    @NotNull
     @Column(nullable = false)
     private LocalDate birthDate;
 
